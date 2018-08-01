472b2880c33c374f11a0e3ce9c990fa17a841d71
Alison Not <alisonfgalvani@gmail.com>
2018-08-01 18:26:11 -0300

    menu efect

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,15 @@ function openNav()
 { 
     var x = document.getElementById('navigation');
     var y = document.getElementById('threeline-icon');
+    var z = document.getElementById('capa');
     
     if (x.className === 'navigation') {
             x.className += ' menujs';
             y.innerHTML = "&Cross;";
+            z.className += ' capaAnime'
     } else {
         x.className = 'navigation';
         y.innerHTML =  'Menu &#9776;';
+        z.className = 'capa'
     }
 }
