7f7faabe325023b57a7bbb5a62fb844d1f66de2d
Alison Not <alisonfgalvani@gmail.com>
2018-08-01 21:08:14 -0300

    Ajuste de animacao domenu em todas as telas

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 
-function openNav()
+function openNav(name)
 { 
     var x = document.getElementById('navigation');
     var y = document.getElementById('threeline-icon');
-    var z = document.getElementById('capa');
+    var z = document.getElementById('conteudoMove');
     
     if (x.className === 'navigation') {
             x.className += ' menujs';
             y.innerHTML = "&Cross;";
-            z.className += ' capaAnime'
+            z.className += ' mainAnimeOn'
     } else {
         x.className = 'navigation';
         y.innerHTML =  'Menu &#9776;';
-        z.className = 'capa'
+        z.className = name
     }
 }
