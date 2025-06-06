637cc45fa33d1d5d74f583407c8d0ad7b6840306
Alison Galvani <alisonfgalvani@gmail.com>
2025-06-05 23:26:09 -0300

    Repaginação e inclusão de novos projetos

--- js/index.js
+++ js/index.js
@@ -26,8 +26,44 @@ function spawnCode() {
     codeBg.appendChild(div)
     setTimeout(() => div.remove(), 10000)
 }
+function animation2Start() {
+    setInterval(spawnCode, 150)
+}
 
-// setInterval(spawnCode, 150)
+
+function animationStart() {
+    const canvas = document.getElementById('matrix')
+    const ctx = canvas.getContext('2d')
+
+    canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+
+    const fontSize = 14
+    const columns = canvas.width / fontSize
+    const drops = Array(Math.floor(columns)).fill(1)
+
+    setInterval(drawMatrix, 33, ctx, canvas, drops, fontSize)
+}
+
+function drawMatrix(ctx, canvas, drops, fontSize) {
+    const letters = '01ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = '#05f2db'
+    ctx.font = `${fontSize}px monospace`
+
+    for (let i = 0; i < drops.length; i++) {
+        const text = letters[Math.floor(Math.random() * letters.length)]
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize)
+
+        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+            drops[i] = 0
+        }
+
+        drops[i]++
+    }
+}
 
 function openNav()
 {
